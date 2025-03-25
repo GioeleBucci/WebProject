2d89b04d611348338bb5f8f8802ad7a581a9370d
Samuele Balducci <samuele.balducci2@studio.unibo.it>
2025-03-25 18:59:34 +0100

    Fixed typos and translation errors in database creation file

--- db/dbCreate.sql
+++ db/dbCreate.sql
@@ -1,8 +1,8 @@
 -- Database Section
 -- ________________ 
 
-create database TEST;
-use TEST;
+create database IF NOT EXISTS test;
+use test;
 
 
 -- Tables Section
@@ -15,12 +15,12 @@ create table ARTICLE (
      material char(16) not null,
      weight int,
      size char(16),
-     constraint IDARTICOLO primary key (articleId));
+     constraint IDARTICLE primary key (articleId));
 
 create table CATEGORY (
      categoryId char(16) not null,
      name char(16) not null,
-     constraint IDCATEGORIA primary key (categoryId));
+     constraint IDCATEGORY primary key (categoryId));
 
 create table CUSTOMER (
      name char(16) not null,
@@ -32,12 +32,12 @@ create table ADDRESS (
      civicNumber int not null,
      city char(32) not null,
      postalCode int not null,
-     constraint IDINDIRIZZO primary key (address, civicNumber, postalCode));
+     constraint IDADDRESS primary key (address, civicNumber, postalCode));
 
 create table PAYMENT_METHOD (
      paymentMethodId char(16) not null,
      name char(16) not null,
-     constraint IDMETODO_PAGAMENTO primary key (paymentMethodId));
+     constraint IDPAYMENT_METHOD primary key (paymentMethodId));
 
 create table ORDER (
      orderId char(16) not null,
@@ -46,20 +46,20 @@ create table ORDER (
      notes char(128),
      tracking char not null,
      refunded char not null,
-     constraint IDORDINE primary key (orderId));
+     constraint IDORDER primary key (orderId));
 
 create table DELIVERY (
      deliveyId char(16) not null,
      departure date not null,
      arrival date,
      lastPosition char(16),
-     constraint IDSPEDIZIONE primary key (deliveyId));
+     constraint IDDELIVERY primary key (deliveyId));
 
 create table USER (
-     email varchar(32) not null,
-     password varchar(16) not null,
+     email char(32) not null,
+     password char(16) not null,
      phoneNumber bigint,
-     constraint IDUTENTE primary key (email));
+     constraint IDUSER primary key (email));
 
 create table SELLER (
      name char(32) not null,
@@ -70,7 +70,7 @@ create table ARTICLE_VERSION (
      features char(128) not null,
      price int not null,
      availability int not null,
-     constraint IDVERSIONE_ARTICOLO primary key (, ));
+     constraint IDARTICLE_VERSION primary key (versionId));
 
 
 -- Constraints Section
