beff30bdacaf1908964aef45e77920b61311dfc3
GioeleBucci <gioele.bucci@studio.unibo.it>
2025-03-28 21:34:53 +0100

    tweak data sizes in db

--- db/dbCreate.sql
+++ db/dbCreate.sql
@@ -3,18 +3,18 @@ use test;
 
 create table if not exists test.CATEGORY (
      categoryId int not null AUTO_INCREMENT,
-     name char(16) null,
+     name char(127) null,
      primary key (categoryId)
 )
 engine = InnoDB;
 
 create table if not exists test.ARTICLE (
      articleId int not null AUTO_INCREMENT,
-     name char(32) null,
-     description char(128) null,
-     material char(16) null,
-     weight int null,
-     size char(16) null,
+     name char(31) null,
+     description char(127) null,
+     material char(255) null,
+     weight decimal(6,2) null,
+     size char(31) null,
      categoryId int not null,
      primary key (articleId),
      constraint FK_ArticleCategory foreign key (categoryId) references test.CATEGORY(categoryId)
@@ -24,7 +24,7 @@ engine = InnoDB;
 create table if not exists test.ARTICLE_VERSION (
      versionId int not null AUTO_INCREMENT,
      articleId int not null,
-     features char(128) null,
+     features char(255) null,
      price int null,
      availability int null,
      primary key (versionId),
@@ -34,25 +34,25 @@ engine = InnoDB;
 
 create table if not exists test.PAYMENT_METHOD (
      paymentMethodId int not null AUTO_INCREMENT,
-     name char(16) null,
+     name char(15) null,
      primary key (paymentMethodId)
 )
 engine = InnoDB;
 
 create table if not exists test.USER (
-     email char(32) not null,
-     password char(16) null,
+     email char(31) not null,
+     password char(31) null,
      isSeller boolean null,
-     address char(64) null,
-     phoneNumber char(16) null,
-     name char(32) null, 
+     address char(63) null,
+     phoneNumber char(31) null,
+     name char(31) null, 
      birthDate date null,
      primary key (email)
 )
 engine = InnoDB;
 
 create table if not exists test.CUSTOMER (
-     email char(32) not null,
+     email char(31) not null,
      paymentMethodId int not null,
      primary key (email),
      constraint FK_CustomerKey foreign key (email) references test.USER(email),
@@ -60,12 +60,11 @@ create table if not exists test.CUSTOMER (
 )
 engine = InnoDB;
 
-
 create table if not exists test.ORDER (
      orderId int not null AUTO_INCREMENT,
-     email char(32) not null,
+     email char(31) not null,
      orderDate date null,
-     notes char(128) null,
+     notes char(127) null,
      primary key (orderId),
      constraint FK_OrderIssuer foreign key (email) references test.CUSTOMER(email) 
 )
@@ -86,17 +85,16 @@ create table if not exists test.DELIVERY (
      orderId int not null,
      departure date null,
      arrival date null,
-     lastPosition char(16) null,
+     lastPosition char(255) null,
      primary key (deliveyId),
      constraint FK_Order foreign key (orderId) references test.ORDER(orderId)
 )
 engine = InnoDB;
 
 create table if not exists test.SELLER (
-     email char(32) not null,
+     email char(31) not null,
      deliveredOrders int null,
      primary key (email),
      constraint FK_SellerKey foreign key (email) references test.USER(email)
 )
 engine = InnoDB;
-
