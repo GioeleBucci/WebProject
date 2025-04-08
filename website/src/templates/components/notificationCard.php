b7b320b818b0f5a57b5708df09ffa95a28f32d56
Samuele Balducci <samuele.balducci2@studio.unibo.it>
2025-04-08 19:25:11 +0200

    added notification component

---
+++ website/src/templates/components/notificationCard.php
@@ -0,0 +1,10 @@
+<div class="card-wrapper">
+    <div class="card h-100">
+        <div class="card-top" style="transform: rotate(0);">
+            <div class="card-body">
+                <h6 class="card-text mb-0 product-name"><?php echo $notification["receptionTime"] ?></h6>
+                <p class="card-text mb-0 text-truncate"><?php echo $notification["content"] ?></p>
+            </div>
+        </div>
+    </div>
+</div>
