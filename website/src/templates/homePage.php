090cfdddfbe518cfde4b44e5993b687074bf4f0b
Samuele Balducci <samuele.balducci2@studio.unibo.it>
2025-04-10 17:40:41 +0200

    small file name fix

--- website/src/templates/homePage.php
+++ website/src/templates/homePage.php
@@ -65,7 +65,7 @@
     <div class="row mt-3 g-3">
         <?php foreach ($dbh->getArticles(50) as $prod): ?>
             <div class="col-6 col-md-3">
-                <?php require("components/itemCard.php") ?>
+                <?php require("components/carouselItemCard.php") ?>
             </div>
         <?php endforeach; ?>
     </div>
