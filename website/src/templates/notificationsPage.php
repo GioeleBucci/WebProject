a9673f7389316eb31017a28f2d45600508017b41
Samuele Balducci <samuele.balducci2@studio.unibo.it>
2025-04-10 17:41:56 +0200

    general refactoring; added a prototype banner when there are no notifications

--- website/src/templates/notificationsPage.php
+++ website/src/templates/notificationsPage.php
@@ -5,39 +5,43 @@
 Utils::requireLoggedUser();
 
 $maxNotificationAmount = 8;         // could be moved to a more appropriate location, e.g. Settings.php
-$notifications = $dbh->getNotifications($_SESSION["sessionId"]);
+$notifications = $dbh->getNotifications($_SESSION["userId"]);
 $notificationsAmount = count($notifications);
-if($notificationsAmount <= $maxNotificationAmount){
-    $sectionsAmount = 1;
-}
-else{
+if ($notificationsAmount > $maxNotificationAmount) {
     $sectionsAmount = $notificationsAmount / $maxNotificationAmount;
-    if($notificationsAmount % $maxNotificationAmount != 0){
+    if ($notificationsAmount % $maxNotificationAmount != 0) {
         $sectionsAmount = $sectionsAmount + 1;
     }
 }
-for($i = 1; $i <= $sectionsAmount; $i = $i +1){
+else {
+    $sectionsAmount = 1;
+}
+for ($i = 1; $i <= $sectionsAmount; $i = $i +1) {
     $sections[$i] = array_slice($notifications, ($i - 1) * $maxNotificationAmount, $maxNotificationAmount);
 }
-if(!isset($_GET["sectionNumber"])){
+if (!isset($_GET["sectionNumber"])) {
     $_GET["sectionNumber"] = 1;
 }
 
 ?>
 
-<div class="container mt-2 mt-md-4">
-    <div class="row">
-        <div class="col">
-            <?php foreach ($sections[$_GET["sectionNumber"]] as $notification): ?>
-                <?php require("components/notificationCard.php") ?>
-            <?php endforeach; ?>
-            <nav>
-                <form>
-                    <?php foreach (array_keys($sections) as $sectionNumber): ?>
-                        <input type="submit" name="sectionNumber" value=<?php echo $sectionNumber ?>></input>
-                    <?php endforeach; ?>
-                </form>
-            </nav>
+<?php if ($notificationsAmount != 0): ?>
+    <div class="container mt-2 mt-md-4">
+        <div class="row">
+            <div class="col">
+                <?php foreach ($sections[$_GET["sectionNumber"]] as $notification): ?>
+                    <?php require("components/notificationCard.php") ?>
+                <?php endforeach; ?>
+                <nav>
+                    <form>
+                        <?php foreach (array_keys($sections) as $sectionNumber): ?>
+                            <input type="submit" name="sectionNumber" value=<?php echo $sectionNumber ?>></input>
+                        <?php endforeach; ?>
+                    </form>
+                </nav>
+            </div>
         </div>
-	</div>
-</div>
+    </div>
+<?php else: ?>
+    <p>There are no notifications</p>
+<?php endif; ?>
