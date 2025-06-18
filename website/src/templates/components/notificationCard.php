bbf55589af45ff9da308dad93b3030f57af89aa2
GioeleBucci <gioele.bucci@studio.unibo.it>
2025-06-18 15:38:46 +0200

    change date format

--- website/src/templates/components/notificationCard.php
+++ website/src/templates/components/notificationCard.php
@@ -4,7 +4,7 @@
             <div class="d-flex justify-content-between align-items-start">
                 <div class="flex-grow-1 me-3">
                     <p class="card-text mb-1"><?php echo $notification["content"] ?></p>
-                    <small class="text-muted"><?php echo $notification["receptionTime"] ?></small>
+                    <small class="text-muted"><?php echo date('d-m-Y H:i', strtotime($notification["receptionTime"])) ?></small>
                 </div>
                 <div class="flex-shrink-0">
                     <form method="POST" class="d-inline">
